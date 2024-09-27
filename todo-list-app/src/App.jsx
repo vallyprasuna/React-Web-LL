701bef1808de6a71c829a98c20ce12e9ab78dd28
Shaun Wassell <shaun.p.wassell@gmail.com>
2024-09-27 18:37:11 +0000

    Added a todo list item component

--- todo-list-app/src/App.jsx
+++ todo-list-app/src/App.jsx
@@ -1,10 +1,19 @@
+import { useState } from 'react';
 import './App.css'
 import TodoList from './TodoList'
 
 function App() {
+  const [completedTodos, setCompletedTodos] = useState([
+    { text: 'Take out the garbage', isCompleted: true },
+    { text: 'Make dinner', isCompleted: true }
+  ]);
+  const [incompleteTodos, setIncompleteTodos] = useState([
+    { text: 'Paint the house', isCompleted: false }
+  ]);
+
   return (
     <>
-    <TodoList completedTodos={[]} incompleteTodos={[]}/>
+    <TodoList completedTodos={completedTodos} incompleteTodos={incompleteTodos}/>
     </>
   );
 }
--- todo-list-app/src/TodoList.jsx
+++ todo-list-app/src/TodoList.jsx
@@ -1,12 +1,18 @@
+import TodoListItem from "./TodoListItem"
+
 export default function TodoList({ completedTodos, incompleteTodos }) {
   return (
     <div>
       <h1>My Todos</h1>
       <p>New todo form will go here...</p>
       <h3>Completed:</h3>
-      
+      {completedTodos.map((todo, index) => (
+        <TodoListItem todo={todo} key={index} />
+      ))}
       <h3>Incomplete:</h3>
-
+      {incompleteTodos.map((todo, index) => (
+        <TodoListItem todo={todo} key={index} />
+      ))}
     </div>
   )
 }
