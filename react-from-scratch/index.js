a677c39ca37498ac16f6fcd977bec167ce40d7a5
Shaun Wassell <shaun.p.wassell@gmail.com>
2024-09-27 16:27:54 +0000

    Added a React script with JSX

--- react-from-scratch/index.js
+++ react-from-scratch/index.js
@@ -1,1 +1,11 @@
-console.log('This is the React rendering script!');
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+
+const App = () => {
+  return <h1>This is a React Script!</h1>
+}
+
+const container = document.getElementById('root');
+const root = createRoot(container);
+
+root.render(<App />)
