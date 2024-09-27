01df4072921d749a1dbc8e1888025ebf271fbd3b
Shaun Wassell <shaun.p.wassell@gmail.com>
2024-09-27 16:57:07 +0000

    Added an App component in a separate file

--- react-from-scratch/index.js
+++ react-from-scratch/index.js
@@ -1,11 +1,8 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-
-const App = () => {
-  return <h1>This is a React Script!</h1>
-}
+import App from './App';
 
 const container = document.getElementById('root');
 const root = createRoot(container);
 
-root.render(<App />)
+root.render(<App message="Hey there! You're now using a component from another file!" />)
