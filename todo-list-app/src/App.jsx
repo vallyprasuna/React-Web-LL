2a020792ab365c86ff053b2e51abae5ee125398d
Shaun Wassell <shaun.p.wassell@gmail.com>
2024-09-27 19:15:57 +0000

    Completed the example app with useState hooks

--- todo-list-app/src/App.jsx
+++ todo-list-app/src/App.jsx
@@ -7,13 +7,32 @@ function App() {
     { text: 'Take out the garbage', isCompleted: true },
     { text: 'Make dinner', isCompleted: true }
   ]);
+
   const [incompleteTodos, setIncompleteTodos] = useState([
     { text: 'Paint the house', isCompleted: false }
   ]);
 
+  function markTodoAsComplete(text) {
+    setIncompleteTodos(incompleteTodos.filter(t => t.text !== text));
+    setCompletedTodos([...completedTodos, { ...incompleteTodos.find(t => t.text === text), isCompleted: true }]);
+  }
+
+  function deleteTodo(text) {
+    setCompletedTodos(completedTodos.filter(t => t.text !== text));
+  }
+
+  function createTodo(text) {
+    setIncompleteTodos([...incompleteTodos, { text, isCompleted: false }]);
+  }
+
   return (
     <>
-    <TodoList completedTodos={completedTodos} incompleteTodos={incompleteTodos}/>
+    <TodoList
+      completedTodos={completedTodos}
+      incompleteTodos={incompleteTodos}
+      onCompletedClicked={markTodoAsComplete}
+      onDeleteClicked={deleteTodo}
+      onCreateClicked={createTodo} />
     </>
   );
 }
