32c8cc7e3d0c25dc67f82e000a788dc38f981fe6
Shaun Wassell <shaun.p.wassell@gmail.com>
2024-09-27 18:25:28 +0000

    Created a new todo list app and the todo list component

---
+++ todo-list-app/src/TodoList.jsx
@@ -0,0 +1,12 @@
+export default function TodoList({ completedTodos, incompleteTodos }) {
+  return (
+    <div>
+      <h1>My Todos</h1>
+      <p>New todo form will go here...</p>
+      <h3>Completed:</h3>
+      
+      <h3>Incomplete:</h3>
+
+    </div>
+  )
+}
