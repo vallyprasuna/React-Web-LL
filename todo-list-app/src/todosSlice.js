7d2623b68110e942a81bbea7111a5bf14751b498
Shaun Wassell <shaun.p.wassell@gmail.com>
2024-09-30 17:30:14 +0000

    Added reducer logic

--- todo-list-app/src/todosSlice.js
+++ todo-list-app/src/todosSlice.js
@@ -6,8 +6,20 @@ const todosSlice = createSlice({
     value: [],
   },
   reducers: {
-    createTodo: state => state,
-    markTodoAsCompleted: state => state,
-    deleteTodo: state => state,
+    createTodo: state => {
+      state.value = [...state.value, {
+        text: 'New Todo',
+        isCompleted: false,
+      }];
+    },
+    markTodoAsCompleted: state => {
+      const text = 'New Todo';
+      const todo = state.value.find(t => t.text === text);
+      todo.isCompleted = true;
+    },
+    deleteTodo: state => {
+      const text = 'New Todo';
+      state.value = state.value.filter(t => t.text !== text);
+    },
   }
 })
