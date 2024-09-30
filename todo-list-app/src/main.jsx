0ade6d5a6c9dcadc9456a14ee39d05b14623db7b
Shaun Wassell <shaun.p.wassell@gmail.com>
2024-09-30 16:10:30 +0000

    Added redux to react

--- todo-list-app/src/main.jsx
+++ todo-list-app/src/main.jsx
@@ -1,10 +1,18 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { configureStore } from '@reduxjs/toolkit';
+import { Provider } from 'react-redux';
 import App from './App.jsx'
 import './index.css'
 
+const store = configureStore({
+  reducer: {},
+});
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </StrictMode>,
 )
