64bd9250d3c466bd43ad9fc88791534c520fd82d
Shaun Wassell <shaun.p.wassell@gmail.com>
2024-10-02 19:46:54 +0000

    Added a styled component

--- todo-list-app/src/TodoListItem.jsx
+++ todo-list-app/src/TodoListItem.jsx
@@ -1,16 +1,23 @@
 import { useDispatch } from "react-redux";
 import { deleteTodo, markTodoAsCompleted } from "./thunks";
+import styled from 'styled-components';
+
+const CardContainer = styled.div`
+  border-radius: 10px;
+  box-shadow: 0 2px 5px rgba(0, 0, 0, 0.5);
+  padding: 16px;
+`;
 
 export default function TodoListItem({ todo }) {
   const dispatch = useDispatch();
 
   return (
-    <div>
+    <CardContainer>
       <h3>{todo.text}</h3>
       {todo.isCompleted && <p>Complete!</p>}
       {todo.isCompleted
         ? <button onClick={() => dispatch(deleteTodo(todo.id))}>Delete Item</button>
         : <button onClick={() => dispatch(markTodoAsCompleted(todo.id))}>Mark as Completed</button>}
-    </div>
+    </CardContainer>
   );
 }
