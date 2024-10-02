278fa5992a1f967f091973e22ca80ae8f35c301f
Shaun Wassell <shaun.p.wassell@gmail.com>
2024-10-02 23:49:07 +0000

    Added a test for styled components

--- todo-list-app/src/TodoListItem.jsx
+++ todo-list-app/src/TodoListItem.jsx
@@ -2,8 +2,16 @@ import { useDispatch } from "react-redux";
 import { deleteTodo, markTodoAsCompleted } from "./thunks";
 import styled from 'styled-components';
 
+const getBackgroundColor = ({ important }) => {
+  if (important) {
+    return 'background-color: yellow;';
+  } else {
+    return 'background-color: white;';
+  }
+}
+
 const CardContainer = styled.div`
-  ${props => props.important && 'background-color: yellow;'}
+  ${getBackgroundColor}
   border-radius: 10px;
   box-shadow: 0 2px 5px rgba(0, 0, 0, 0.5);
   padding: 16px;
