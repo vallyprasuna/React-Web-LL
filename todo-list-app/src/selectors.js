fc8301d271d1025487cb4013c1aace0ea30313a7
Shaun Wassell <shaun.p.wassell@gmail.com>
2024-10-02 19:37:11 +0000

    Used the createSelector function

--- todo-list-app/src/selectors.js
+++ todo-list-app/src/selectors.js
@@ -1,12 +1,7 @@
+import { createSelector } from "@reduxjs/toolkit";
+
 export const getTodos = state => state.todos.value;
 export const getTodosLoading = state => !state.loading.value.completed;
 
-export const getCompletedTodos = state => {
-  const todos = getTodos(state);
-  return todos.filter(t => t.isCompleted);
-}
-
-export const getIncompleteTodos = state => {
-  const todos = getTodos(state);
-  return todos.filter(t => !t.isCompleted);
-}
+export const getCompletedTodos = createSelector([getTodos], todos => todos.filter(t => t.isCompleted));
+export const getIncompleteTodos = createSelector([getTodos], todos => todos.filter(t => !t.isCompleted));
