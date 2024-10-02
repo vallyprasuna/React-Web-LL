c491275aba1305e9c70ad4c712bd20600bd24043
Shaun Wassell <shaun.p.wassell@gmail.com>
2024-10-02 16:16:09 +0000

    Added a todo to the server

--- todo-list-app/server.js
+++ todo-list-app/server.js
@@ -3,7 +3,11 @@ import express from 'express';
 const app = express();
 const port = 3000;
 
-let todos = [];
+let todos = [{
+  id: '123',
+  text: 'Add a server',
+  isCompleted: true,
+}];
 
 app.use(express.json());
 
