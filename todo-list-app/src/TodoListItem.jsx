b84abdef0027e98b420868267d64d236c4ef94ba
Shaun Wassell <shaun.p.wassell@gmail.com>
2024-10-02 23:27:40 +0000

    Added a styled component

--- todo-list-app/src/TodoListItem.jsx
+++ todo-list-app/src/TodoListItem.jsx
@@ -3,6 +3,7 @@ import { deleteTodo, markTodoAsCompleted } from "./thunks";
 import styled from 'styled-components';
 
 const CardContainer = styled.div`
+  ${props => props.important && 'background-color: yellow;'}
   border-radius: 10px;
   box-shadow: 0 2px 5px rgba(0, 0, 0, 0.5);
   padding: 16px;
@@ -12,7 +13,7 @@ export default function TodoListItem({ todo }) {
   const dispatch = useDispatch();
 
   return (
-    <CardContainer>
+    <CardContainer important={todo.text.endsWith('!')}>
       <h3>{todo.text}</h3>
       {todo.isCompleted && <p>Complete!</p>}
       {todo.isCompleted
